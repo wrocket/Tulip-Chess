379692e1beae19f0e9a9360723ede6d0bd411262
Brian Wray <brian.the.wray@gmail.com>
2015-04-09 17:53:08 -0500

    removing unused #include

--- src/makemove.c
+++ src/makemove.c
@@ -20,8 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#include <stdio.h>
-
 #include "tulip.h"
 #include "piece.h"
 #include "bitboard.h"
