d0d92fe45e61ce32a1d6793d92c58896ebe466a9
Brian Wray <brian.the.wray@gmail.com>
2015-06-11 16:30:37 -0500

    Adding/fixing book building utilities. Adding script to dump an opening book to a SQLite file.

---
+++ utils/build_book.py
@@ -0,0 +1,97 @@
+#!/usr/local/bin/python3
+
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 Brian Wray (brian@wrocket.org)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# This script reads a digest of a PGN file and stores the moves in a SQLite3
+# database indexed by the position hash.
+#
+# The idea is to use this in conjunction with the digest_pgn.py script, e.g.:
+# $ ./digest_pgn.py my_openings.pgn | ./build_book.py
+
+import subprocess
+import json
+import sqlite3 as lite
+import sys
+import re
+import os
+
+
+class MoveLine:
+    def __init__(self, initial_hash, move_hashes):
+        self.initial_hash = initial_hash
+        self.move_hashes = move_hashes
+
+
+def build_line(line):
+    cmd = ['../src/tulip', '-hashseq', 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1']
+    cmd.extend(line)
+    out = subprocess.check_output(cmd)
+    result = json.loads(out.decode('utf-8'))
+    return MoveLine(result['initialHash'], [(x['move'], x['resultingHash']) for x in result['hashSequence']])
+
+
+def digest_line(move_line):
+    d = {}
+    current_hash = move_line.initial_hash
+    for item in move_line.move_hashes:
+        d[current_hash] = [item[0]]
+        current_hash = item[1]
+    return d
+
+
+def combine_lines(line_dicts):
+    result = {}
+    for d in line_dicts:
+        for (k, v) in d.items():
+            if k not in result:
+                result[k] = set(v)
+            else:
+                result[k].update(v)
+    return result
+
+
+def write_to_database(data, database_name):
+    if os.path.exists(database_name):
+        os.remove(database_name)
+    with lite.connect(database_name) as con:
+        cur = con.cursor()
+        cur.execute('create table OPENING_BOOK(POSITION_HASH VARCHAR2(16), MOVE VARCHAR2(8))')
+        cur.execute('create index POSITION_IDX on OPENING_BOOK(POSITION_HASH)')
+        move_count = 0
+        for (position, moves) in data.items():
+            move_count += len(moves)
+            for m in moves:
+                cur.execute('insert into OPENING_BOOK values (?, ?)', (position, m))
+        print('Wrote %i moves(s) and %i unique position(s) to opening file "%s"' % (move_count, len(data.keys()), database_name))
+
+
+def read_input():
+    raw_lines = [line.strip() for line in sys.stdin]
+    return list([re.split(r'\s+', line) for line in raw_lines])
+
+
+all_lines = read_input()
+print("Read %i line(s) from stdin." % len(all_lines))
+digested_lines = [digest_line(build_line(x)) for x in all_lines]
+file_lines = combine_lines(digested_lines)
+write_to_database(file_lines, 'tulip_openings.db')
--- utils/digest_pgn.py
+++ utils/digest_pgn.py
@@ -1,17 +1,19 @@
+#!/usr/local/bin/python3
+
 # The MIT License (MIT)
 #
 # Copyright (c) 2015 Brian Wray (brian@wrocket.org)
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,7 +24,12 @@
 
 # Script to digest a series of PGN files in to a simple list of move strings.
 # The output can be used to easily build up transposition tables or opening
-# books in the main C code.
+# books in the main C code or elsewhere.
+
+# The output is a simple list of moves to the given depth found in the PGN.
+# No attempt to parse or otherwise validate the move strings is made, so
+# checking for illegal moves needs to be done by the consumer of this
+# output.
 
 import sys
 import re
@@ -30,63 +37,66 @@ from itertools import chain
 
 _max_moves = 20
 
-if len(sys.argv) <= 2:
-	print('Usage: %s [output file] [pgn file 1] [pgn file 2] ...' % sys.argv[0])
-	exit(-1)
+if len(sys.argv) <= 1:
+    print('Usage: %s [pgn file 1] [pgn file 2] ...' % sys.argv[0])
+    exit(-1)
 
 
+# Turn a token like '1.e4' to 'e4'
+def remove_move_numbers(t):
+    m = re.match(r'^\d+\.(.*)', t)
+    return m.group(1) if m else t
+
+
+# Eliminate empty tokens and move number tokens.
 def is_move_token(str):
-	return len(str) and not re.match(r'^\d+\.*', str)
+    return len(str) and not re.match(r'^\d+\.*', str)
 
 
 def parse_moves(game_lines):
-	joined_lines = ' '.join(game_lines)
-	comments_removed = re.sub(r'{.*?}', '', joined_lines)
-	split = re.split('\s+', comments_removed)
-	move_strs = filter(lambda s: is_move_token(s), split)
-	return list(move_strs)[:_max_moves]
+    joined_lines = ' '.join(game_lines)
+    comments_removed = re.sub(r'{.*?}', '', joined_lines)
+    split = re.split('\s+', comments_removed)
+    no_numbers = [remove_move_numbers(t) for t in split]
+    move_strs = filter(lambda s: is_move_token(s), no_numbers)
+    return list(move_strs)[:_max_moves]
 
 
 def parse_pgn_lines(line_source):
-	results = []
-	inside_game = False
-	game_lines = []
-	for line in line_source:
-		line = line.strip()
-		if len(line) == 0 or line.startswith('['):
-			if inside_game and len(game_lines):
-				results.append(parse_moves(game_lines))
-				game_lines = []
-			inside_game = False
-			continue
-		elif inside_game:
-			game_lines.append(line)
-		else:
-			inside_game = True
-			game_lines = [line]
-	if len(game_lines):
-		results.append(parse_moves(game_lines))
-	return results
+    results = []
+    inside_game = False
+    game_lines = []
+    for line in line_source:
+        line = line.strip()
+        if len(line) == 0 or line.startswith('['):
+            if inside_game and len(game_lines):
+                results.append(parse_moves(game_lines))
+                game_lines = []
+            inside_game = False
+            continue
+        elif inside_game:
+            game_lines.append(line)
+        else:
+            inside_game = True
+            game_lines = [line]
+    if len(game_lines):
+        results.append(parse_moves(game_lines))
+    return results
 
 
 def read_pgn_file(file_name):
-	print('Reading file %s...' % file_name)
-	with open(file_name, 'r') as in_file:
-		results = parse_pgn_lines(in_file)
-	print('\t...found %i game(s) in %s.' % (len(results), file_name))
-	return results
+    with open(file_name, 'r') as in_file:
+        results = parse_pgn_lines(in_file)
+    return results
 
 
 def write_digest_file(results):
-	flattened = chain.from_iterable(results)
-	non_empty = filter(lambda r: len(r) > 0, flattened)
-	lines = set([' '.join(r) for r in non_empty])
-	with open(sys.argv[1], 'w') as output_file:
-		print('Writing %i distinct game line(s) to %s...' % (len(lines), sys.argv[1]))
-		for l in sorted(lines):
-			output_file.write('%s\n' % l)
-	print('\t...done.')
+    flattened = chain.from_iterable(results)
+    non_empty = filter(lambda r: len(r) > 0, flattened)
+    lines = set([' '.join(r) for r in non_empty])
+    for l in sorted(lines):
+        print(l)
 
-print('Limiting line length to %i move(s)' % _max_moves)
-all_games = [read_pgn_file(f) for f in sys.argv[2:]]
+
+all_games = [read_pgn_file(f) for f in sys.argv[1:]]
 write_digest_file(all_games)
