d4e653dbc3f3c8fed28c74837cad9aa031640f65
Brian Wray <brian@wrocket.org>
2016-03-27 20:24:16 -0500

    Tabs.

--- src/attack.c
+++ src/attack.c
@@ -33,112 +33,112 @@
 #include "tulip.h"
 
 static bool slide(const int32_t startSq, const int32_t offset, const Piece** board, const Piece* slider, const Piece* queen) {
-        const Piece* p;
-        int32_t q = startSq;
+	const Piece* p;
+	int32_t q = startSq;
 
-        do {
-                q += offset;
-                p = board[q];
-        }
-        while (p == &EMPTY);
+	do {
+		q += offset;
+		p = board[q];
+	}
+	while (p == &EMPTY);
 
-        return p == slider || p == queen;
+	return p == slider || p == queen;
 }
 
 bool isCheck(GameState* state) {
-        StateData* data = state->current;
-        return data->toMove == COLOR_WHITE ?
-               canAttack(COLOR_BLACK, data->whiteKingSquare, state) :
-               canAttack(COLOR_WHITE, data->blackKingSquare, state);
+	StateData* data = state->current;
+	return data->toMove == COLOR_WHITE ?
+	       canAttack(COLOR_BLACK, data->whiteKingSquare, state) :
+	       canAttack(COLOR_WHITE, data->blackKingSquare, state);
 }
 
 bool canAttack(const int32_t color, const int32_t sq, GameState* state) {
-        uint64_t mask;
-        const Piece* knight;
-        const Piece* bishop;
-        const Piece* rook;
-        const Piece* queen;
-        const Piece* king;
-        const Piece* pawn;
-        uint64_t pawnMask;
-        bool result;
-        const Piece** b = state->board;
+	uint64_t mask;
+	const Piece* knight;
+	const Piece* bishop;
+	const Piece* rook;
+	const Piece* queen;
+	const Piece* king;
+	const Piece* pawn;
+	uint64_t pawnMask;
+	bool result;
+	const Piece** b = state->board;
 
 #define BITS(p) state->bitboards[(p)->ordinal]
 
-        if (color == COLOR_BLACK) {
-                rook = &BROOK;
-                bishop = &BBISHOP;
-                queen = &BQUEEN;
-                king = &BKING;
-                knight = &BKNIGHT;
-                pawn = &BPAWN;
-                pawnMask = BITS_BPAWN[sq];
-        } else {
-                rook = &WROOK;
-                bishop = &WBISHOP;
-                king = &WKING;
-                queen = &WQUEEN;
-                knight = &WKNIGHT;
-                pawn = &WPAWN;
-                pawnMask = BITS_WPAWN[sq];
-        }
+	if (color == COLOR_BLACK) {
+		rook = &BROOK;
+		bishop = &BBISHOP;
+		queen = &BQUEEN;
+		king = &BKING;
+		knight = &BKNIGHT;
+		pawn = &BPAWN;
+		pawnMask = BITS_BPAWN[sq];
+	} else {
+		rook = &WROOK;
+		bishop = &WBISHOP;
+		king = &WKING;
+		queen = &WQUEEN;
+		knight = &WKNIGHT;
+		pawn = &WPAWN;
+		pawnMask = BITS_WPAWN[sq];
+	}
 
-        // Check for pawn attacks.
-        if (BITS(pawn) & pawnMask) {
-                return true;
-        }
+	// Check for pawn attacks.
+	if (BITS(pawn) & pawnMask) {
+		return true;
+	}
 
-        // Look for knight attacks.
-        if (BITS(knight) & BITS_KNIGHT[sq]) {
-                return true;
-        }
+	// Look for knight attacks.
+	if (BITS(knight) & BITS_KNIGHT[sq]) {
+		return true;
+	}
 
-        // If an opposing rook or queen is on the file or rank...
-        mask = BITS_ROOK[sq];
-        if ((BITS(rook) & mask) || BITS(queen) & mask) {
-                result = slide(sq, OFFSET_N, b, rook, queen)
-                         || slide(sq, OFFSET_S, b, rook, queen)
-                         || slide(sq, OFFSET_E, b, rook, queen)
-                         || slide(sq, OFFSET_W, b, rook, queen);
+	// If an opposing rook or queen is on the file or rank...
+	mask = BITS_ROOK[sq];
+	if ((BITS(rook) & mask) || BITS(queen) & mask) {
+		result = slide(sq, OFFSET_N, b, rook, queen)
+		         || slide(sq, OFFSET_S, b, rook, queen)
+		         || slide(sq, OFFSET_E, b, rook, queen)
+		         || slide(sq, OFFSET_W, b, rook, queen);
 
-                if (result) {
-                        return true;
-                }
-        }
+		if (result) {
+			return true;
+		}
+	}
 
-        mask = BITS_BISHOP[sq];
-        if ((BITS(bishop) & mask) || BITS(queen) & mask) {
-                result = slide(sq, OFFSET_NE, b, bishop, queen)
-                         || slide(sq, OFFSET_NW, b, bishop, queen)
-                         || slide(sq, OFFSET_SE, b, bishop, queen)
-                         || slide(sq, OFFSET_SW, b, bishop, queen);
+	mask = BITS_BISHOP[sq];
+	if ((BITS(bishop) & mask) || BITS(queen) & mask) {
+		result = slide(sq, OFFSET_NE, b, bishop, queen)
+		         || slide(sq, OFFSET_NW, b, bishop, queen)
+		         || slide(sq, OFFSET_SE, b, bishop, queen)
+		         || slide(sq, OFFSET_SW, b, bishop, queen);
 
-                if (result) {
-                        return true;
-                }
-        }
+		if (result) {
+			return true;
+		}
+	}
 
-        // Look for king attacks.
-        if (BITS(king) & BITS_KING[sq]) {
-                return true;
-        }
+	// Look for king attacks.
+	if (BITS(king) & BITS_KING[sq]) {
+		return true;
+	}
 
 #undef BITS
 
-        // No pawn attacks, no rook attacks, no bishop attacks, or knight attacks, or queen attacks...
-        // Guess they can't attack.
-        return false;
+	// No pawn attacks, no rook attacks, no bishop attacks, or knight attacks, or queen attacks...
+	// Guess they can't attack.
+	return false;
 }
 
 bool isLegalPosition(GameState* state) {
-        bool result;
+	bool result;
 
-        if (state->current->toMove == COLOR_WHITE) {
-                result = !canAttack(COLOR_WHITE, state->current->blackKingSquare, state);
-        } else {
-                result = !canAttack(COLOR_BLACK, state->current->whiteKingSquare, state);
-        }
+	if (state->current->toMove == COLOR_WHITE) {
+		result = !canAttack(COLOR_WHITE, state->current->blackKingSquare, state);
+	} else {
+		result = !canAttack(COLOR_BLACK, state->current->whiteKingSquare, state);
+	}
 
-        return result;
+	return result;
 }
--- src/draw.c
+++ src/draw.c
@@ -33,93 +33,93 @@
 // If all the bishops are on the same color, and each side has at least
 // one bishop, and there are no other pieces, this is a draw.
 static inline bool moreThanFourPieces(GameState* g, const int32_t total) {
-    bool result = false;
-    const int32_t countWb = g->pieceCounts[ORD_WBISHOP];
-    const int32_t countBb = g->pieceCounts[ORD_BBISHOP];
+	bool result = false;
+	const int32_t countWb = g->pieceCounts[ORD_WBISHOP];
+	const int32_t countBb = g->pieceCounts[ORD_BBISHOP];
 
-    if (countWb && countBb && (countWb + countBb + 2) == total) {
-        const uint64_t wbishop = g->bitboards[ORD_WBISHOP];
-        const uint64_t bbishop = g->bitboards[ORD_BBISHOP];
+	if (countWb && countBb && (countWb + countBb + 2) == total) {
+		const uint64_t wbishop = g->bitboards[ORD_WBISHOP];
+		const uint64_t bbishop = g->bitboards[ORD_BBISHOP];
 
-        const bool wbishopsOnLight = (BIT_SQUARES_LIGHT & wbishop) != 0;
-        const bool wbishopsOnDark = (BIT_SQUARES_DARK & wbishop) != 0;
-        const bool bbishopsOnLight = (BIT_SQUARES_LIGHT & bbishop) != 0;
-        const bool bbishopsOnDark = (BIT_SQUARES_DARK & bbishop) != 0;
+		const bool wbishopsOnLight = (BIT_SQUARES_LIGHT & wbishop) != 0;
+		const bool wbishopsOnDark = (BIT_SQUARES_DARK & wbishop) != 0;
+		const bool bbishopsOnLight = (BIT_SQUARES_LIGHT & bbishop) != 0;
+		const bool bbishopsOnDark = (BIT_SQUARES_DARK & bbishop) != 0;
 
-        const bool allWBishopsOnSameColor = wbishopsOnLight ^ wbishopsOnDark;
-        const bool allBBishopsOnSameColor = bbishopsOnLight ^ bbishopsOnDark;
-        const bool allBishopsOnSameColor = ((wbishopsOnLight && bbishopsOnLight) || (wbishopsOnDark && bbishopsOnDark));
+		const bool allWBishopsOnSameColor = wbishopsOnLight ^ wbishopsOnDark;
+		const bool allBBishopsOnSameColor = bbishopsOnLight ^ bbishopsOnDark;
+		const bool allBishopsOnSameColor = ((wbishopsOnLight && bbishopsOnLight) || (wbishopsOnDark && bbishopsOnDark));
 
-        result = allWBishopsOnSameColor && allBBishopsOnSameColor && allBishopsOnSameColor;
-    }
+		result = allWBishopsOnSameColor && allBBishopsOnSameColor && allBishopsOnSameColor;
+	}
 
-    return result;
+	return result;
 }
 
 // Deal with KB vs KB with bishops on same square color.
 static inline bool fourPieces(GameState* g, const int32_t total) {
-    bool result = false;
+	bool result = false;
 
-    if (g->pieceCounts[ORD_WBISHOP] == 1 && g->pieceCounts[ORD_BBISHOP] == 1) {
-        const uint64_t wbishop = g->bitboards[ORD_WBISHOP];
-        const uint64_t bbishop = g->bitboards[ORD_BBISHOP];
-        result = ((BIT_SQUARES_LIGHT & wbishop) && (BIT_SQUARES_LIGHT & bbishop))
-                 || ((BIT_SQUARES_DARK & wbishop) && (BIT_SQUARES_DARK & bbishop));
-    }
+	if (g->pieceCounts[ORD_WBISHOP] == 1 && g->pieceCounts[ORD_BBISHOP] == 1) {
+		const uint64_t wbishop = g->bitboards[ORD_WBISHOP];
+		const uint64_t bbishop = g->bitboards[ORD_BBISHOP];
+		result = ((BIT_SQUARES_LIGHT & wbishop) && (BIT_SQUARES_LIGHT & bbishop))
+				 || ((BIT_SQUARES_DARK & wbishop) && (BIT_SQUARES_DARK & bbishop));
+	}
 
-    return result;
+	return result;
 }
 
 static inline bool threePieces(GameState* g, const int32_t total) {
-    // If there are three pieces, two must be kings.
-    // That means there's one non-king piece.
-    // If that one piece is a knight or bishop of any color, we're in a draw.
-    return g->pieceCounts[ORD_WBISHOP] > 0
-           || g->pieceCounts[ORD_BBISHOP] > 0
-           || g->pieceCounts[ORD_WKNIGHT] > 0
-           || g->pieceCounts[ORD_WKNIGHT] > 0;
+	// If there are three pieces, two must be kings.
+	// That means there's one non-king piece.
+	// If that one piece is a knight or bishop of any color, we're in a draw.
+	return g->pieceCounts[ORD_WBISHOP] > 0
+		   || g->pieceCounts[ORD_BBISHOP] > 0
+		   || g->pieceCounts[ORD_WKNIGHT] > 0
+		   || g->pieceCounts[ORD_WKNIGHT] > 0;
 }
 
 bool draw_isMaterial(GameState* g) {
-    const int32_t total = g->current->whitePieceCount + g->current->blackPieceCount;
-    bool result;
-    switch (total) {
-    case 4: // Exactly four pieces looks for KBvKB with same-color bishops.
-        result = fourPieces(g, total);
-        break;
-    case 3:
-        result = threePieces(g, total); // Look for KNvK or KBvK
-        break;
-    case 2:
-        result = true; // Two pieces means KvK, a draw.
-        break;
-    default:
-        result = moreThanFourPieces(g, total); // Look for kings and bishops where bishops are same-color.
-        break;
-    }
+	const int32_t total = g->current->whitePieceCount + g->current->blackPieceCount;
+	bool result;
+	switch (total) {
+	case 4: // Exactly four pieces looks for KBvKB with same-color bishops.
+		result = fourPieces(g, total);
+		break;
+	case 3:
+		result = threePieces(g, total); // Look for KNvK or KBvK
+		break;
+	case 2:
+		result = true; // Two pieces means KvK, a draw.
+		break;
+	default:
+		result = moreThanFourPieces(g, total); // Look for kings and bishops where bishops are same-color.
+		break;
+	}
 
-    return result;
+	return result;
 }
 
 bool draw_isThreefold(GameState* g) {
-    StateData* currentData = g->current;
-    const uint64_t startingHash = currentData->hash;
-    bool result = false;
-    int32_t repeatCount = 0;
-    const int limit = MIN(currentData->fiftyMoveCount, currentData->halfMoveCount);
+	StateData* currentData = g->current;
+	const uint64_t startingHash = currentData->hash;
+	bool result = false;
+	int32_t repeatCount = 0;
+	const int limit = MIN(currentData->fiftyMoveCount, currentData->halfMoveCount);
 
-    for (int32_t i = 0; i < limit; i++) {
-        currentData--;
-        const uint64_t currentHash = currentData->hash;
-        // Hash changes with toMove, so compare the hash with either white to move or black to move.
-        if (currentHash == startingHash || (currentHash ^ HASH_WHITE_TO_MOVE) == startingHash) {
-            repeatCount++;
-            if (repeatCount == 2) {
-                result = true;
-                break;
-            }
-        }
-    }
+	for (int32_t i = 0; i < limit; i++) {
+		currentData--;
+		const uint64_t currentHash = currentData->hash;
+		// Hash changes with toMove, so compare the hash with either white to move or black to move.
+		if (currentHash == startingHash || (currentHash ^ HASH_WHITE_TO_MOVE) == startingHash) {
+			repeatCount++;
+			if (repeatCount == 2) {
+				result = true;
+				break;
+			}
+		}
+	}
 
-    return result;
+	return result;
 }
